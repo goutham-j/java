7726beafef85589a7018de5c9ecbe992c26b1a9a
Goutham J <54531058+goutham-j@users.noreply.github.com>
2024-07-14 12:47:33 +0530

    Added For, Stack & Array functions

--- src/main/java/com/javaexamples/ReverseWords.java
+++ src/main/java/com/javaexamples/ReverseWords.java
@@ -3,25 +3,80 @@ import java.util.*;
 
 public class ReverseWords {
 	
-	public String reverseWords(String s) {
+	// reverse words using for loop
+	public String reverseWordsFor(String s) {
 
+		//split the sentence on blank space
+		String words[] = s.split("\\s");
+		String rWord = "";
+		int i;
+		
+		for (i = words.length-1; i >= 0; i--) { 
+			rWord += words[i] + " "; 
+		}
+	     
+        rWord = rWord.substring(0, rWord.length() - 1); //remove the empty char at the end
+	    return rWord;
+	}
+	
+	// reverse words using for loop
+	public String reverseWordsStack(String s) {
+
+		//split the sentence on blank space
 		String words[] = s.split(" ");
 		String rWord = "";
-		Stack<String> wordStack = new Stack<String>();
+		Stack<String> wordStack = new Stack<>();
 		
-		for (String w:words) { wordStack.push(w); }
+		for (String w:words) { 
+			wordStack.push(w); 
+		}
 		
-        while (!wordStack.isEmpty()) { rWord += wordStack.pop() + " "; }
+        while (!wordStack.isEmpty()) { 
+        	rWord += wordStack.pop() + " "; 
+        }
 	     
+        rWord = rWord.substring(0, rWord.length() - 1); //remove the empty char at the end
+	    return rWord;
+	}
+	
+	public String reverseWordsArray(String s) {
+		
+		String word = "";
+		String rWord = "";
+		Stack<String> wordStack = new Stack<>();
+		int i;
+		
+		for (i=0; i < s.length(); i++) {
+			if (s.charAt(i) == ' ') {
+				wordStack.push(word);
+				word = "";
+			} else {
+				word = word + s.charAt(i);
+			}
+		}
+		wordStack.push(word);
+		
+        while (!wordStack.isEmpty()) {
+        	rWord += wordStack.pop() + " ";
+        }
+        
+        rWord = rWord.substring(0, rWord.length() - 1); //remove the empty char at the end
+        
 	    return rWord;
 	}
 
 	public static void main(String[] args) {
-		String str = "My name is Goutham";
-		
-		System.out.println("Original String: " + str);
+		String tmp;
+		String str = "I love Java programming";
 		ReverseWords rs = new ReverseWords();
-		System.out.println("Reversed String: " + rs.reverseWords(str));
+		
+		System.out.println("Original String: " + str + ", Size: " + str.length());
+		tmp = rs.reverseWordsFor(str);
+		System.out.println("Reversed String - For  : " + tmp + ", Size: " + tmp.length());
+		tmp = rs.reverseWordsStack(str);
+		System.out.println("Reversed String - Stack: " + tmp + ", Size: " + tmp.length());
+		tmp = rs.reverseWordsArray(str);
+		System.out.println("Reversed String - Array: " + tmp + ", Size: " + tmp.length());
 
 	}
 
